4118ce108b9c244e4506749c67b99e0290ee6dd0
Fayez Morsel <fayezmorsel77@gmail.com>
2025-07-25 15:11:56 +0300

    add comment

--- app.js
+++ app.js
@@ -15,3 +15,262 @@ function save() {
 }
 
 console.log("Let's count people on the subway!");
+
+
+// // 1. Create a variable, myAge, and set its value to your age
+// let myAge = 35
+
+// // 2. Log the myAge variable to the console
+// console.log(myAge)
+
+// document.getElementById("count").innerText = 5
+
+// cmd+k+c
+// let firstBatch = 5
+// let secondBatch = 7
+// let count = firstBatch + secondBatch
+// console.log(count)
+
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+
+// let myAge = 35
+// let humanDogRatio = 7
+
+// let myDogAge = myAge * humanDogRatio
+
+// console.log(myDogAge)
+
+
+// document.getElementById("count").innerText = 5
+
+// let count = 5
+// count = count + 1
+// console.log(count)
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
+
+// let bonusPoints = 50
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+
+// document.getElementById("count").innerText = 5
+
+// let count = 5
+// count = count + 1
+// console.log(count)
+
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+// function increment() {
+//     console.log("The button was clicked")
+// }
+
+// function countdown() {
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
+
+// // Setting up the the race 🏎 🏎 🏎
+
+// countdown()
+
+// // GO! 🏁
+// // Players are running the race 🏎 💨
+// // Race is finished! 🍾
+
+// // Get ready for a new race 🏎 🏎 🏎
+
+// countdown()
+
+
+// function myLogger() {
+//     console.log(42)
+// }
+
+// myLogger()
+
+
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// // Create a function that logs out the sum of all the lap times
+// function logLapTime() {
+//     let totalTime = lap1 + lap2 + lap3
+//     console.log(totalTime)
+// }
+
+// console.log(totalTime)
+
+// //logLapTime()
+
+
+// let lapsCompleted = 0
+
+// // Create a function that increments the lapsCompleted variable with one
+// // Run it three times
+
+// function incrementLap() {
+//     lapsCompleted = lapsCompleted + 1
+// }
+
+// incrementLap()
+// incrementLap()
+// incrementLap()
+
+// console.log(lapsCompleted)
+
+// document.getElementById("count").innerText = 5
+
+// let count = 5
+// count = count + 1
+// console.log(count)
+
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked (log it out)
+// change the count-el in the HTML to reflect the new count
+
+// let count = 0
+
+// function increment() {
+//     count = count + 1
+//     console.log(count)
+// }
+
+// let countEl = document.getElementById("count-el")
+// console.log(countEl)
+// let count = 0
+// function increment() {
+//     count = count + 1
+//     countEl.innerText = count
+// }
+
+// function save() {
+//     console.log(count)
+// }
+
+// let username = "per"
+// let message = "You have tree new notifications"
+
+// let messageToUser = message + ", " + username + "!"
+
+// console.log(messageToUser)
+
+// let name = "Per Harald Borgen"
+// let greeting = "Hi, my name is "
+// let myGreeting = greeting + name
+// console.log(myGreeting)
+
+// let name = 42
+// let greeting = "Hi, my name is "
+// let myGreeting = greeting + name
+// // console.log(myGreeting)
+
+// // let points = 4
+// // let bonusPoints = "10"
+
+// // let totalPoints = points + bonusPoints
+
+// console.log(4 + 5) // 9
+// console.log("2" + "4") // "24"
+// console.log("5" + 1) // "51"
+// console.log(100 + "100") // "100100"
+
+// let countEl = document.getElementById("count-el")
+// let count = 0
+
+// function increment() {
+//     // Change this to use the plus equal technique you've learned
+//     count += 1
+//     countEl.innerText = count
+// }
+
+// function save() {
+//     console.log(count)
+// }
+
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+// let saveEl = document.getElementById("save-el")
+// let countEl = document.getElementById("count-el")
+// let count = 0
+
+// console.log(saveEl)
+
+// function increment() {
+//     count += 1
+//     countEl.innerText = count
+// }
+
+// function save() {
+//     let countStr = count + " - "
+//     // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+//     // 3. Render the variable in the saveEl using innerText
+//     saveEl.innerText += countStr
+//     // NB: Make sure to not delete the existing content of the paragraph
+//     console.log(count)
+// }
+
+
+// // 1. Grab the save-el paragrah and store it in a variable called saveEl
+// let saveEl = document.getElementById("save-el")
+// let countEl = document.getElementById("count-el")
+// let count = 0
+
+// console.log(saveEl)
+
+// function increment() {
+//     count += 1
+//     countEl.textContent = count
+// }
+
+// function save() {
+//     let countStr = count + " - "
+//     // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+//     // 3. Render the variable in the saveEl using innerText
+//     saveEl.textContent += countStr
+//     // NB: Make sure to not delete the existing content of the paragraph
+//     console.log(count)
+// }
+
+// let saveEl = document.getElementById("save-el")
+// let countEl = document.getElementById("count-el")
+// let count = 0
+
+// function increment() {
+//     count += 1
+//     countEl.textContent = count
+// }
+
+// function save() {
+//     let countStr = count + " - "
+//     saveEl.textContent += countStr
+//     countEl.textContent = 0
+//     count = 0
+// }
+
+
+
+
+
+
